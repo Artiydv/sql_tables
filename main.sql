4f2648d5e1e61b8ae757353cae3b7d98ebf2e4a2
Artiydv <119195583+Artiydv@users.noreply.github.com>
2023-06-30 00:17:45 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,15 +1,15 @@
 -- create a table
-CREATE TABLE Github (
+CREATE TABLE Gitcustomer (
   Customer Segment TEXT PRIMARY KEY,
-   Y_2016(M) integer,
-   Y_2017(M) integer,
-   Y_2018(M) integer,
-   Y_2019(M) integer,
-   Y_2020(M) integer,
-   Y_2022(M) integer
+   Y_2016 varchar,
+   Y_2017 varchar,
+   Y_2018 varchar,
+   Y_2019 varchar,
+   Y_2020 varchar,
+   Y_2022 varchar
 );
 -- insert some values
-INSERT INTO Github VALUES ('Developers',5.8 ,24,31,40,56,94);
-INSERT INTO Github VALUES ('Organizations', 0.3, 1.5,2.5,2.9,0,0);
+INSERT INTO Gitcustomer VALUES ('Developers',"5.8M" ,"24M","31M","40M","56M","94M");
+INSERT INTO Gitcustomer VALUES ('Organizations', "0.3M", "1.5M","2.5M","2.9M","NA","NA");
 -- fetch some values
-SELECT * FROM Github ;
+SELECT * FROM Gitcustomer ;
